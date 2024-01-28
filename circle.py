5aa97d109f96db41c17a5832531c3baf20f0c547
Gayathri1295 <gayathriganesan12@outlook.com>
2024-01-28 01:27:37 -0500

    added a python file to calculate area of a circle

---
+++ circle.py
@@ -0,0 +1,12 @@
+#Function circle where r is the radius
+def circle(r):
+    pi = 3.14
+    
+    #area of the circle
+    area = (r**2) * pi
+    
+    #printing the result
+    print("The area of the circle is" , area)
+
+#Calling function circle with radius = 5
+circle(5)
